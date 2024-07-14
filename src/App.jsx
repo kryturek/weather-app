bca772272933c26063bb72139a8c331046592a5e
kryturek <75915136+kryturek@users.noreply.github.com>
2024-07-14 19:36:17 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+	const api = {
+		key: "fa7fce5a165ee9ae4a181a41305fd2a6",
+		baseurlgeo: "http://api.openweathermap.org/geo/1.0/direct?",
+		baseurlweather: "https://api.openweathermap.org/data/2.5/weather?",
+	};
+
+	const [location, setLocation] = useState("");
+	const [geoData, setGeoData] = useState({});
+	const [weather, setWeather] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	function processLocation(ev) {
+		ev.preventDefault();
+		setLoading(true);
+		setError(null);
+		if (weather.name != undefined) {
+			sessionStorage.setItem("lastSearched", weather.name);
+		}
+		// sessionStorage.removeItem("lastSearched");
+		fetch(`${api.baseurlgeo}q=${location}&limit=1&appid=${api.key}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setGeoData(data);
+			});
+
+		fetch(`${api.baseurlweather}q=${location}&units=metric&appid=${api.key}`)
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.cod == "404") {
+					setError(`${location} not found!`);
+				} else {
+					setWeather(data);
+				}
+				setLoading(false);
+			})
+			.catch(setLoading(false));
+	}
+
+	return (
+		<div className="App">
+			<header>
+				<h1>Weather App 2</h1>
+				<form>
+					<input
+						type="text"
+						placeholder="enter location!"
+						value={location}
+						onChange={(ev) => setLocation(ev.target.value)}
+					/>
+					<button onClick={processLocation}>Search</button>
+				</form>
+
+				{sessionStorage.getItem("lastSearched") ? (
+					<span className="lastSearched">
+						Last searched: {sessionStorage.getItem("lastSearched")}
+					</span>
+				) : (
+					""
+				)}
+				{/* <p>Last searched: {sessionStorage.getItem("lastSearched")}</p> */}
+			</header>
+
+			{loading ? (
+				<div class="lds-dual-ring"></div>
+			) : error ? (
+				<p>{error}</p>
+			) : weather.main ? (
+				<div className="weather">
+					<div className="overview">
+						<div className="overviewLocation">
+							<p className="locationInfo li-name">{geoData[0].name}</p>
+							<div className="stateAndCountry">
+								<p className="locationInfo">
+									{geoData[0].state}, {geoData[0].country}
+								</p>
+							</div>
+						</div>
+						<img
+							className="weatherIcon"
+							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+							alt=""
+						/>
+						<p className="temperatureInfo">
+							{Math.round(weather.main.temp)}°C
+						</p>
+					</div>
+					<div className="info">
+						<p>Feels like {Math.round(weather.main.feels_like)}°C</p>
+						<p>{weather.weather[0].main}</p>
+						<p>({weather.weather[0].description})</p>
+					</div>
+				</div>
+			) : (
+				<p></p>
+			)}
+		</div>
+	);
+}
+
+export default App;
